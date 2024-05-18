1f3e89dd4390f1643760645e1921f07b3ba687d7
Ziwei <u7733544@anu.edu.au>
2024-05-19 07:23:19 +1000

    added api key

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,13 +1,15 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
+// Initialize the Resend client with your API key
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req, res) {
+export async function POST(req) {
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
   try {
+    // Send the email using the Resend API
     const data = await resend.emails.send({
       from: fromEmail,
       to: [fromEmail, email],
@@ -23,6 +25,7 @@ export async function POST(req, res) {
     });
     return NextResponse.json(data);
   } catch (error) {
+    // Handle any errors that occur during the request
     return NextResponse.json({ error });
   }
 }
