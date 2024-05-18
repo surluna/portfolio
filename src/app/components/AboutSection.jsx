436217dc3b77db7a3cb96f732a5f6e2d780775f8
Ziwei <u7733544@anu.edu.au>
2024-05-19 07:50:19 +1000

    fixed info

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -9,12 +9,16 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <ul className="list-disc pl-2">
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>PostgreSQL</li>
-        <li>Sequelize</li>
+        <li>HTML</li>
+        <li>CSS</li>
         <li>JavaScript</li>
-        <li>React</li>
+        <li>Node.js</li>
+        <li>MongoDB</li>
+        <li>Express</li>
+        <li>SQL</li>
+        <li>Java</li>
+        <li>Python</li>
+        <li>Git</li>
       </ul>
     ),
   },
@@ -23,18 +27,26 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Fullstack Academy of Code</li>
-        <li>University of California, Santa Cruz</li>
+        <li style={{ fontWeight: 600 }}>Master of Computing</li>
+        <li style={{ listStyleType: 'none' }}>Australian National University</li>
+        <li style={{ listStyleType: 'none' }}>Canberra, Australia</li>
+        <br />
+        <li style={{ fontWeight: 600 }}>Bachelor of Management</li>
+        <li style={{ listStyleType: 'none' }}>Hunan Normal University</li>
+        <li style={{ listStyleType: 'none' }}>Hunan, China</li>
       </ul>
     ),
   },
   {
-    title: "Certifications",
-    id: "certifications",
+    title: "Internships",
+    id: "Internships",
     content: (
       <ul className="list-disc pl-2">
-        <li>AWS Cloud Practitioner</li>
-        <li>Google Professional Cloud Developer</li>
+        <li style={{ fontWeight: 600 }}>Strategy Analyst</li>
+        <li style={{ listStyleType: 'none' }}>ByteDance<br />(TikTok Parent Company)</li>
+        <br />
+        <li style={{ fontWeight: 600 }}>Livestreaming Operations</li>
+        <li style={{ listStyleType: 'none' }}>NetEase</li>
       </ul>
     ),
   },
@@ -53,16 +65,11 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
+        <Image src="/images/about-image.png" width={500} height={500} alt="About Image" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+            I am a full stack web developer with a passion for creating interactive and responsive web applications. Interactive and responsive web applications. I have Working with HTML, CSS, JavaScript, Node.js, Express, MongoDB, Java, Python, SQL and Git. I am a fast learner and I am always looking to expand my knowledge and skills. I am a team player and I am excited to work with others to create amazing applications.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
@@ -80,14 +87,14 @@ const AboutSection = () => {
               Education{" "}
             </TabButton>
             <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
+              selectTab={() => handleTabChange("Internships")}
+              active={tab === "Internships"}
             >
               {" "}
-              Certifications{" "}
+              Internships{" "}
             </TabButton>
           </div>
-          <div className="mt-8">
+          <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-4">
             {TAB_DATA.find((t) => t.id === tab).content}
           </div>
         </div>
